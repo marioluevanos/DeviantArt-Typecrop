7df2a131b8d3a6af38852e517698c110191b8b95
Mario Luevanos <mario@magical-threads.com>
2020-04-03 16:16:57 -0700

    Use let

--- src/fontFace.js
+++ src/fontFace.js
@@ -2,11 +2,11 @@ import { eot, woff } from './base64Fonts'
 const fontFamily = 'Calibre-Bold'
 
 export default function fontFace() {
-    var fontFace = '';
+    let fontFace = '';
     fontFace += '@font-face {';
     fontFace += 'font-family: ' + fontFamily + ';';
     fontFace += `src: url("data:font/opentype; base64, ${ eot }") format("embedded-opentype");`;
     fontFace += `src: url("data:application/x-font-woff; base64, ${ woff }") format("woff");`;
     fontFace += '}';
     return fontFace;
-};
+}
